05dbeeaccacfc9e0be644d60a8e07824d6500cc9
Osman Tonbul <osmantonbul2001@gmail.com>
2026-03-15 01:51:14 +0300

    Add project files.

---
+++ Stock.API/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+var app = builder.Build();
+
+
+
+app.Run();
+
